0251f054730f0b657e92aa6bc73702fc3e5facf2
macmou <julien.rahon15@gmail.com>
2026-06-04 14:25:07 +0200

    feat(ui):mep-home-1

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,14 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Coupe du monde de football 2026 : vos pronostiques
+          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Coupe du monde de football 2026
           </h1>
+          <h2>Faites vos pronostiques maintenant !</h2>
+          <img src="/coupe-du-monde-2026.jpg" alt="logo" />
           <p>Faites vos pronostiques sur les matchs de coupe du monde 2026</p>
-          <h2>Règles du jeu :</h2>
+          <h3>Règles du jeu :</h3>
           <ul className="list-disc list-inside text-left">
             <li>Choisissez les équipes qui vont se rencontrer</li>
             <li>Prévoyez le score exact du match</li>
